4e23c1f8770d2b90bf2178939ff978bc324f0c74
abandonfail <3240529156@qq.com>
2025-08-02 15:15:46 +0800

    应用模块-后端

---
+++ src/main/java/com/aicodegenerate/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.aicodegenerate.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.aicodegenerate.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author 陈
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
