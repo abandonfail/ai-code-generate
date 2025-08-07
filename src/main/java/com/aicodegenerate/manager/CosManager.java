7c99ea55594a999e31d6826f904bdeb3549cec76
abandonfail <3240529156@qq.com>
2025-08-07 16:20:31 +0800

    功能扩展 - 应用封面图

--- src/main/java/com/aicodegenerate/manager/CosManager.java
+++ src/main/java/com/aicodegenerate/manager/CosManager.java
@@ -10,12 +10,14 @@ import com.qcloud.cos.model.PutObjectResult;
 import com.qcloud.cos.model.ciModel.persistence.PicOperations;
 
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class CosManager {
 
@@ -35,6 +37,27 @@ public class CosManager {
         PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key,
                 file);
         return cosClient.putObject(putObjectRequest);
+    }
+
+    /**
+     * 上传文件到 COS 并返回访问 URL
+     *
+     * @param key  COS对象键（完整路径）
+     * @param file 要上传的文件
+     * @return 文件的访问URL，失败返回null
+     */
+    public String uploadFile(String key, File file) {
+        // 上传文件
+        PutObjectResult result = putObject(key, file);
+        if (result != null) {
+            // 构建访问URL
+            String url = String.format("%s%s", cosClientConfig.getHost(), key);
+            log.info("文件上传COS成功: {} -> {}", file.getName(), url);
+            return url;
+        } else {
+            log.error("文件上传COS失败，返回结果为空");
+            return null;
+        }
     }
 
     /**
--- src/main/java/com/aicodegenerate/service/impl/AppServiceImpl.java
+++ src/main/java/com/aicodegenerate/service/impl/AppServiceImpl.java
@@ -20,6 +20,7 @@ import com.aicodegenerate.model.vo.AppVO;
 import com.aicodegenerate.model.vo.UserVO;
 import com.aicodegenerate.service.AppService;
 import com.aicodegenerate.service.ChatHistoryService;
+import com.aicodegenerate.service.ScreenshotService;
 import com.aicodegenerate.service.UserService;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
@@ -61,6 +62,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
 
     @Resource
     private VueProjectBuilder vueProjectBuilder;
+
+    @Resource
+    private ScreenshotService screenshotService;
 
     @Override
     public Flux<String> chatToGenCode(Long appId, String message, User loginUser) {
@@ -141,9 +145,34 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         updateApp.setDeployedTime(LocalDateTime.now());
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
-        // 10. 返回可访问的 URL 地址
-        return String.format("%s/%s", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 10. 构建应用访问 URL
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 11. 异步生成截图并更新应用封面
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
     }
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+    @Override
+    public void generateAppScreenshotAsync(Long appId, String appUrl) {
+        // 使用虚拟线程异步执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新应用封面字段
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
+    }
+
 
     @Override
     public AppVO getAppVO(App app) {
